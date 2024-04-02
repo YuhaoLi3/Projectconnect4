2757917911e619b3c2df75c49a3d358e1e1b40f9
BT_1N3_26 <Yguan1996@bths,edu>
2024-04-02 09:30:27 -0400

    Finished cleaning and making GameOverDisplay

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -2,6 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class MainRunner {
     public static void main(String[] args) {
-
+        ConnectFourGame game  = new ConnectFourGame();
+        game.start();
     }
 }
