61b776e6cf1a167239ea18532bead1224b92e2fe
Yuhao Li <Yuhaoguan12@gmail.com>
2024-04-08 01:30:37 -0400

    cleaned

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,5 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class MainRunner {
     public static void main(String[] args) {
             ConnectFourGame game = new ConnectFourGame();
--- src/Players.java
+++ src/Players.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 public class Players extends GamePiece {
         public Players(String symbol) {
             super(symbol);
